31d3c9cb5dbb1323355dcf559744b7c06487799c
yash-gamdha <yashgamdha97@gmail.com>
2025-11-03 22:50:25 +0530

    use expressive motion-scheme

--- app/src/main/java/com/app/ringtonerandomizer/ui/theme/Theme.kt
+++ app/src/main/java/com/app/ringtonerandomizer/ui/theme/Theme.kt
@@ -3,7 +3,9 @@ package com.app.ringtonerandomizer.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -33,6 +35,7 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun RingtoneRandomizerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -53,6 +56,7 @@ fun RingtoneRandomizerTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        motionScheme = MotionScheme.expressive()
     )
 }
