f6aecba724f97c7296d332f4842eb80a6581b913
yash-gamdha <yashgamdha97@gmail.com>
2025-11-05 20:08:34 +0530

    return animated background

--- app/src/main/java/com/app/ringtonerandomizer/presentation/home_screen/components/getGradientBackground.kt
+++ app/src/main/java/com/app/ringtonerandomizer/presentation/home_screen/components/getGradientBackground.kt
@@ -1,17 +1,45 @@
 package com.app.ringtonerandomizer.presentation.home_screen.components
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun getGradientBackground() =
-    Brush.horizontalGradient(
-        colors = listOf(
-            Color.Transparent,
-            MaterialTheme.colorScheme.tertiaryContainer,
-            MaterialTheme.colorScheme.secondaryContainer,
-            Color.Transparent
+fun getGradientBackground(): Brush {
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val color1 = infiniteTransition.animateColor(
+        initialValue = Color.Transparent,
+        targetValue = MaterialTheme.colorScheme.tertiaryContainer,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 3 * 1000),
+            repeatMode = RepeatMode.Reverse
         )
     )
+
+    val color2 = infiniteTransition.animateColor(
+        initialValue = MaterialTheme.colorScheme.tertiaryContainer,
+        targetValue = MaterialTheme.colorScheme.secondaryContainer,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 3 * 1000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val color3 = infiniteTransition.animateColor(
+        initialValue = MaterialTheme.colorScheme.secondaryContainer,
+        targetValue = Color.Transparent,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 3 * 1000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    return Brush.horizontalGradient(colors = listOf(color1.value, color2.value, color3.value))
+}
