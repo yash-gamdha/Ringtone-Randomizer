e44257f862b77776dce56ca0b4d2e06287e23d3b
yash-gamdha <yashgamdha97@gmail.com>
2025-11-05 20:09:56 +0530

    replace old UI with new UI

--- app/src/main/java/com/app/ringtonerandomizer/presentation/home_screen/components/RingtoneRow.kt
+++ app/src/main/java/com/app/ringtonerandomizer/presentation/home_screen/components/RingtoneRow.kt
@@ -1,138 +1,141 @@
 package com.app.ringtonerandomizer.presentation.home_screen.components
 
 import android.content.Context
-import androidx.compose.foundation.LocalIndication
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.app.ringtonerandomizer.R
 import com.app.ringtonerandomizer.core.data.GlobalVariables
 import com.app.ringtonerandomizer.core.domain.getRingtoneDuration
 import com.app.ringtonerandomizer.presentation.home_screen.ClickEvents
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.app.ringtonerandomizer.ui.theme.RingtoneRandomizerTheme
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun RingtoneRow(
     ringtone: String,
     currentRingtone: String,
     context: Context,
-    scope: CoroutineScope,
     onClick: (ClickEvents) -> Unit,
     index: Int,
-    isPlaying: Int // current ringtone index which is playing
+    isPlaying: Int, // current ringtone index which is playing
+    modifier: Modifier = Modifier
 ) {
-    // to show the menu
-    var isContextMenuVisible by rememberSaveable {
+    var isExpanded by remember {
         mutableStateOf(false)
-    }
-
-    var pressOffset by remember {
-        mutableStateOf(DpOffset.Zero)
-    }
-
-    var itemHeight by remember {
-        mutableStateOf(0.dp)
-    }
-
-    var dropDownItems = listOf(
-        "Set as ringtone", "Delete"
-    )
-
-    val screenWidth = LocalConfiguration.current.screenWidthDp
-
-    val density = LocalDensity.current
-    val interactionSource = remember {
-        MutableInteractionSource()
     }
 
     // gradient background for current ringtone
     val bgModifier = if (ringtone == currentRingtone) {
-        Modifier.background(
-            brush = getGradientBackground()
-        )
+        modifier.background(brush = getGradientBackground())
     } else {
-        Modifier
+        modifier.background(
+            if (isExpanded) MaterialTheme.colorScheme.surfaceContainer
+            else MaterialTheme.colorScheme.background
+        )
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 8.dp, end = 8.dp),
-        contentAlignment = Alignment.Center
+    val isPlayingBool by derivedStateOf {
+        isPlaying == index
+    }
+
+    val rotateValue by derivedStateOf {
+        if (isPlayingBool) 360F else 0F
+    }
+
+    val animationProgress by animateFloatAsState(
+        targetValue = rotateValue,
+        label = "toggle_play_pause",
+        animationSpec = MaterialTheme.motionScheme.fastSpatialSpec()
+    )
+
+    val playPauseIconVector by derivedStateOf {
+        if (isPlayingBool && rotateValue >= 90) R.drawable.pause else R.drawable.play
+    }
+
+    Column(
+        modifier = bgModifier
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(40.dp)
-                .indication(interactionSource, LocalIndication.current)
-                .onSizeChanged {
-                    itemHeight = with(density) { it.height.toDp() }
+                .clickable {
+                    isExpanded = !isExpanded
                 }
-                .pointerInput(Unit) {
-                    detectTapGestures(
-                        onLongPress = {
-                            isContextMenuVisible = true
-                            pressOffset = DpOffset(it.x.toDp(), it.y.toDp())
-                        },
-                        // for ripple effect
-                        onPress = {
-                            val press = PressInteraction.Press(it)
-                            interactionSource.emit(press)
-                            tryAwaitRelease()
-                            interactionSource.emit(PressInteraction.Release(press))
-                        }
-                    )
-                }
-                .then(bgModifier),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .padding(8.dp)
         ) {
             Text(
                 text = "${index + 1}. $ringtone",
                 overflow = TextOverflow.Ellipsis,
-                modifier = Modifier
-                    .fillMaxWidth(0.75f)
+                modifier = Modifier.weight(3F)
             )
+            Text(
+                text = getRingtoneDuration("${GlobalVariables.PATH}$ringtone"),
+                textAlign = TextAlign.End,
+                modifier = Modifier.weight(1F)
+            )
+        }
+
+        AnimatedVisibility(
+            visible = isExpanded,
+            label = "ringtone_actions",
+            enter = expandVertically(animationSpec = MaterialTheme.motionScheme.fastSpatialSpec()),
+            exit = shrinkVertically(animationSpec = MaterialTheme.motionScheme.fastSpatialSpec()),
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Spacer(Modifier.height(8.dp))
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(MaterialTheme.colorScheme.onPrimaryContainer),
+                horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                Text(
-                    text = getRingtoneDuration("${GlobalVariables.PATH}$ringtone")
-                )
+                IconButton(
+                    onClick = {
+                        onClick(ClickEvents.DeleteRingtone(context, ringtone))
+                    }
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.delete),
+                        contentDescription = "delete $ringtone",
+                        tint = MaterialTheme.colorScheme.surface
+                    )
+                }
+
                 IconButton(
                     onClick = {
                         if (isPlaying == index) {
@@ -143,50 +146,44 @@ fun RingtoneRow(
                     }
                 ) {
                     Icon(
-                        imageVector = ImageVector.vectorResource(
-                            if (isPlaying == index) {
-                                R.drawable.pause
-                            } else R.drawable.play
-                        ),
-                        contentDescription = if (isPlaying == index) {
-                            "Pause button"
-                        } else "Play button"
+                        imageVector = ImageVector.vectorResource(playPauseIconVector),
+                        contentDescription = if (isPlaying != -1) "pause $ringtone" else "play $ringtone",
+                        modifier = Modifier
+                            .rotate(animationProgress),
+                        tint = MaterialTheme.colorScheme.surface
+                    )
+                }
+
+                IconButton(
+                    onClick = {
+                        onClick(ClickEvents.SetRingtone(context, ringtone))
+                    }
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.music),
+                        contentDescription = "set $ringtone as ringtone",
+                        tint = MaterialTheme.colorScheme.surface
                     )
                 }
             }
         }
-        DropdownMenu(
-            expanded = isContextMenuVisible,
-            onDismissRequest = { isContextMenuVisible = false },
-            offset = pressOffset.copy(
-                x = if (pressOffset.x > (screenWidth.dp / 2)) {
-                    (pressOffset.x) - (pressOffset.x / 3)
-                } else {
-                    pressOffset.x
-                },
-                y = pressOffset.y - itemHeight
-            )
-        ) {
-            dropDownItems.forEach {
-                DropdownMenuItem(
-                    onClick = {
-                        scope.launch {
-                            isContextMenuVisible = false
-                            delay(1000L)
-                            onClick(
-                                ClickEvents.DropDownClick(
-                                    option = it,
-                                    context = context,
-                                    ringtone = ringtone
-                                )
-                            )
-                        }
-                    },
-                    text = {
-                        Text(it)
-                    }
-                )
-            }
-        }
+    }
+}
+
+@Composable
+@PreviewLightDark
+private fun RingtoneRowPreview() {
+    RingtoneRandomizerTheme {
+        RingtoneRow(
+            ringtone = "Ringtone 1",
+            currentRingtone = "Ringtone 2",
+            context = LocalContext.current,
+            onClick = { },
+            index = 1,
+            isPlaying = -1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(8.dp))
+        )
     }
 }
