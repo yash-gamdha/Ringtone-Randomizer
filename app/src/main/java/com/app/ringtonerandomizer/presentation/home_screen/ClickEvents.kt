34c3c89bfe67db981e8f43b7d7181ece2a088caa
yash-gamdha <yashgamdha97@gmail.com>
2025-11-05 20:08:21 +0530

    add new actions

--- app/src/main/java/com/app/ringtonerandomizer/presentation/home_screen/ClickEvents.kt
+++ app/src/main/java/com/app/ringtonerandomizer/presentation/home_screen/ClickEvents.kt
@@ -6,8 +6,9 @@ import android.net.Uri
 sealed interface ClickEvents {
     data class ChangeRingtone(val context: Context): ClickEvents
     data class CopyRingtone(val listOfUri: List<Uri>, val context: Context): ClickEvents
-    data class DropDownClick(val context: Context,val option: String, val ringtone: String): ClickEvents
     data class PlayRingtone(val context: Context, val ringtone: String, val index: Int): ClickEvents
     data class PauseRingtone(val ringtone: String, val index: Int): ClickEvents
+    data class SetRingtone(val context: Context, val ringtone: String): ClickEvents
+    data class DeleteRingtone(val context: Context, val ringtone: String): ClickEvents
     data class UpdateSequentialRotationSetting(val context: Context, val value: Boolean): ClickEvents
 }
