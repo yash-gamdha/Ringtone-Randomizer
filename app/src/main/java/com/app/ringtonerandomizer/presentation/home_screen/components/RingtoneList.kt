53a80daa27c7384be8785a4559596f6386d02ab8
yash-gamdha <yashgamdha97@gmail.com>
2025-11-05 20:09:11 +0530

    minor changes

--- app/src/main/java/com/app/ringtonerandomizer/presentation/home_screen/components/RingtoneList.kt
+++ app/src/main/java/com/app/ringtonerandomizer/presentation/home_screen/components/RingtoneList.kt
@@ -2,13 +2,15 @@ package com.app.ringtonerandomizer.presentation.home_screen.components
 
 import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.app.ringtonerandomizer.presentation.home_screen.ClickEvents
-import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun RingtoneList(
@@ -16,25 +18,29 @@ fun RingtoneList(
     currentRingtone: String,
     state: LazyListState,
     context: Context,
-    scope: CoroutineScope,
     isPlaying: Int,
     onDropDownClick: (ClickEvents) -> Unit,
     modifier: Modifier = Modifier
 ) {
     LazyColumn(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(4.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
         state = state
     ) {
-        items(ringtones.size) { index ->
+        items(
+            ringtones.size,
+            key = { it }
+        ) { index ->
             RingtoneRow(
                 ringtone = ringtones[index],
                 currentRingtone = currentRingtone,
                 index = index,
                 context = context,
                 onClick = onDropDownClick,
-                scope = scope,
-                isPlaying = isPlaying
+                isPlaying = isPlaying,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
             )
         }
     }
